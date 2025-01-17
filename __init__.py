72ffdb400718c67cd268548305418f0dbf1c4357
Gnahiet.any <91592659+eosfel97@users.noreply.github.com>
2025-01-17 15:04:40 +0100

    exo5

--- __init__.py
+++ __init__.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-     return render_template("contact.html")
+     return render_template("/hello.html")
 
 
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return render_template("/contact.html")
 
   
 @app.route('/tawarano/')
